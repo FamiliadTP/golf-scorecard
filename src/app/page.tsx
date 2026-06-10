6c591a4b0b8780db23556069ec67e335a5a4a928
Pedro de Tezanos Pinto <ptezanos@gmail.com>
2026-06-10 18:37:19 -0400

    fix: force-dynamic build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+export const dynamic = 'force-dynamic'
 import { useEffect, useState } from 'react'
 import Link from 'next/link'
 import { supabase, Round, Course } from '@/lib/supabase'
